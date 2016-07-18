0fdba2b4e088e11e11e740de18eddc0c8fb51099
Владислав Бакаев <hugiron@MacBook-Air.local>
2016-07-18 14:28:36 +0400

    Add extend of template in panel

--- controller/view.py
+++ controller/view.py
@@ -1,6 +1,6 @@
 from flask import render_template, session
 
 def index():
-    if ('user_id' in session):
-        return render_template('panel/index.html')
-    return render_template('main/index.html')
+    #if ('user_id' in session):
+    return render_template('panel/index.html')
+    #return render_template('main/index.html')
