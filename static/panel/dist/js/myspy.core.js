f2c8e32186b2e2aee5fc248a1559abd34f567b23
Владислав Бакаев <hugiron@MacBook-Air.local>
2016-07-25 01:18:41 +0400

    Get all friends and all groups (fixed)

--- static/panel/dist/js/myspy.core.js
+++ static/panel/dist/js/myspy.core.js
@@ -21,25 +21,34 @@ $.core.request = function(method, params, callback) {
 }
 
 $.core.getFriends = function(params, callback) {
+    var maxCount = 10000;
     params['order'] = 'hints';
     params['fields'] = 'photo_100,online';
-    $.core.request(
-        'friends.get',
-        params,
-        function(data) {
-            callback(data['items']);
-        }
-    );
+    params['offset'] = 0;
+    params['count'] = 5000;
+    for (var i = 0; i < (maxCount - 1) / params['count'] + 1; ++i, params['offset'] += params['count'])
+        alert(params['offset']);
+        $.core.request(
+            'friends.get',
+            params,
+            function(data) {
+                callback(data['items']);
+            }
+        );
 }
 
 $.core.getGroups = function(params, callback) {
+    var maxCount = 5000;
     params['fields'] = 'members_count';
     params['extended'] = 1;
-    $.core.request(
-        'groups.get',
-        params,
-        function(data) {
-            callback(data['items']);
-        }
-    );
+    params['offset'] = 0;
+    params['count'] = 1000;
+    for (var i = 0; i < (maxCount - 1) / params['count'] + 1; ++i, params['offset'] += params['count'])
+        $.core.request(
+            'groups.get',
+            params,
+            function(data) {
+                callback(data['items']);
+            }
+        );
 }
